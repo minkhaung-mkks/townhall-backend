2740f5439170dc1eeafb9edd212e38c818b464dd
min khaung <minkhaung.mkks@gmail.com>
2026-02-26 01:41:28 +0700

    fixed cors

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://mkks-abac.eastus2.cloudapp.azure.com"
+    "https://townhall.eastus2.cloudapp.azure.com"
 ];
 
 const getCorsHeaders = (req) => {
