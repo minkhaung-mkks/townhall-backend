0cadf74b745b450225a83d288526a4b07a4c71f1
Hein <u6632106@au.edu>
2026-02-25 18:38:14 +0700

    add cors.js

---
+++ src/lib/cors.js
@@ -0,0 +1,21 @@
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "https://mkks-abac.eastus2.cloudapp.azure.com"
+];
+
+const getCorsHeaders = (req) => {
+    const origin = req?.headers?.get?.("origin") || "";
+    const allowOrigin = allowedOrigins.includes(origin) ? origin : allowedOrigins[0];
+    return {
+        "Access-Control-Allow-Origin": allowOrigin,
+        "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Max-Age": "86400",
+        "Vary": "Origin"
+    };
+};
+
+export default getCorsHeaders;
