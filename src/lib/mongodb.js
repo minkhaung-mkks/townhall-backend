e04eb008558532ab946c825a378f8af47102a680
Hein <u6632106@au.edu>
2026-02-25 18:37:28 +0700

    add mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,21 +1,19 @@
 import { MongoClient } from "mongodb";
-
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  throw new Error("Please define the MONGODB_URI in .env.local");
-}
-
-let client;
-let clientPromise;
-
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  global._mongoClientPromise = client.connect();
-}
-
-clientPromise = global._mongoClientPromise;
-
-export async function getClientPromise() {
-  return clientPromise;
+const options = {};
+let globalClientPromise;
+export function getClientPromise() {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+        throw new Error("Please add your Mongo URI to .env.local or set MONGODB_URI env variable"); 
+    }
+    if (process.env.NODE_ENV === "development") {
+        if (!globalClientPromise) {
+            const client = new MongoClient(uri, options);
+            globalClientPromise = client.connect();
+        }
+        return globalClientPromise;
+    } else {
+        const client = new MongoClient(uri, options);
+        return client.connect();
+    }
 }
