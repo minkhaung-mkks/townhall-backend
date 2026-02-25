bbab1ce2bcad22d148df1a15651eca6accaca8fe
Hein <u6632106@au.edu>
2026-02-25 18:45:32 +0700

    add route.js for profile

---
+++ src/app/api/user/profile/route.js
@@ -0,0 +1,47 @@
+import { verifyJWT } from "@/lib/auth";
+import corsHeaders from "@/lib/cors";
+import { getClientPromise } from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export async function OPTIONS(req) {
+    return new Response(null, {
+        status: 200,
+        headers: corsHeaders(req),
+    });
+}
+
+export async function GET(req) {
+    const user = verifyJWT(req);
+    if (!user) {
+        return NextResponse.json({
+            message: "Unauthorized"
+        }, {
+            status: 401,
+            headers: corsHeaders(req)
+        });
+    }
+    
+    try {
+        const client = await getClientPromise();
+        const db = client.db("wad-01");
+        const email = user.email;
+        const profile = await db.collection("user").findOne({ email });
+        console.log("profile: ", profile);
+        
+        // Don't return password
+        const { password, ...profileWithoutPassword } = profile;
+        
+        return NextResponse.json(profileWithoutPassword, {
+            headers: corsHeaders(req)
+        });
+    }
+    catch (error) {
+        console.log("Get Profile Exception: ", error.toString());
+        return NextResponse.json({
+            message: error.toString()
+        }, {
+            status: 400,
+            headers: corsHeaders(req)
+        });
+    }
+}
